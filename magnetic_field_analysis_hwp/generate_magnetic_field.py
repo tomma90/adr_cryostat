a4adc8cbee1aae47d170388b0f574b3d7654ee25
Tommaso Ghigna <38280335+tomma90@users.noreply.github.com>
2021-12-02 17:08:23 +0900

    Update generate_magnetic_field.py

--- magnetic_field_analysis_hwp/generate_magnetic_field.py
+++ magnetic_field_analysis_hwp/generate_magnetic_field.py
@@ -1,3 +1,11 @@
 import numpy as np                                                               
 import matplotlib.pyplot as plt                                                  
-import formula as fo                                                             
+import define_magnet as dm
+
+x = np.linspace(0., 1., 10000)
+B = dm.B_ring(1.1e4, 12e-3, 68.5e-3, 58.5e-3, x)
+
+plt.figure()
+plt.plot(x, B)
+
+plt.show()
